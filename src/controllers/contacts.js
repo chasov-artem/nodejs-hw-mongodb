942b352044d9bd74438246701eb652da4bb16f9e
chasov-artem <chasov90@gmail.com>
2024-11-30 20:59:38 +0200

    step4 sort

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,9 +7,12 @@ import {
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export const getContactsCtrl = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  console.log({ sortBy, sortOrder });
   const contacts = await getAllContacts({ page, perPage });
   res.status(200).json({
     status: 200,

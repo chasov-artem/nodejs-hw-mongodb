dedd398ca0bc48e05e861cb19bf9c843f1ae1e20
chasov-artem <chasov90@gmail.com>
2024-12-06 10:22:31 +0200

    session

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,9 @@
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
+import crypto from 'node:crypto';
 
 import { User } from '../models/user.js';
+import { Session } from '../models/session.js';
 
 export const registerUser = async (payload) => {
   const user = await User.findOne({ email: payload.email });
@@ -26,4 +28,14 @@ export const loginUser = async (email, password) => {
   if (isMatch !== true) {
     throw createHttpError(401, 'Email or password is incorrect');
   }
+
+  await Session.deleteOne({ userId: user._id });
+
+  Session.create({
+    userId: user._id,
+    accessToken: crypto.randomBytes(30).toString('base64'),
+    refreshToken: crypto.randomBytes(30).toString('base64'),
+    accessTokenValidUntil: new Date(Date.now() + 15 * 60 * 1000),
+    refreshTokenValidUntil: new Date(Date.now() + 30 * 24 * 60 * 1000),
+  });
 };

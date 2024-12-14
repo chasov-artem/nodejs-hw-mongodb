b14a7ac36870f9d805aa937a2a693be60abe4e91
chasov-artem <chasov90@gmail.com>
2024-12-14 19:06:15 +0200

    cloudinary

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'node:path';
 import cors from 'cors';
 import pino from 'pino-http';
 import contactsRouter from './routers/contacts.js';
@@ -11,6 +12,8 @@ import { auth } from './middlewares/auth.js';
 export const setupServer = () => {
   const app = express();
   const logger = pino();
+
+  app.use('/photos', express.static(path.resolve('src/public/photos')));
 
   app.use(cors());
   app.use(cookieParser());
